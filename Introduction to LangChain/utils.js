3b4c827d6cf45cb1dd9d0ba0b67762014af5670f
Kostas Minaidis <minaidisk@gmail.com>
2025-12-26 23:11:21 +0200

    Formatting

--- Introduction to LangChain/utils.js
+++ Introduction to LangChain/utils.js
@@ -3,12 +3,22 @@ export function displayAgentMessages(messages) {
   console.log("\n======");
 
   for (const message of messages) {
+
     const constructorName = message.constructor.name;
     console.log(`${constructorName}: "${message.content}"`);
+
     if (constructorName === "AIMessage") {
+
+      console.log(constructorName);
+
       if (message.tool_calls.length > 0) {
+
         console.log(`Tool call: ${message.tool_calls[0].name} => args: ${JSON.stringify(message.tool_calls[0].args)}`);
+
       }
+
     }
+
   }
+
 }
